453f15121595245dd5da73178fc9d7ee76cb43ac
Andrej Palicka <palicka@netlife.sk>
2012-05-23 00:41:49 +0200

    oh shit oh fuck

--- big_num.cpp
+++ big_num.cpp
@@ -368,10 +368,10 @@ std::string big_num::to_string() const
 		res += ('0' + temp % 10);
 		temp = temp/10;
 		temp.decimal--;
-		if(temp.decimal ==0)
+		if(temp.decimal == 0)
 			res+='.';
 	}
-	while(temp.decimal != 0)
+	while(temp.decimal != 0 && temp.decimal != (size_t)0-1)
 	{
 		res += "0";
 		temp.decimal--;
